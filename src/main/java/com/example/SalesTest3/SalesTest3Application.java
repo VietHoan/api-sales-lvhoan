26f651deea95b2fd38b06afdae207486eeb8412e
hoanlv <hoanlv>
2022-08-24 16:21:40 +0700

    update

--- src/main/java/com/example/SalesTest3/SalesTest3Application.java
+++ src/main/java/com/example/SalesTest3/SalesTest3Application.java
@@ -2,9 +2,17 @@ package com.example.SalesTest3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SalesTest3Application {
+
+	@GetMapping()
+	public String message(){
+		return "Welcome you to main page";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SalesTest3Application.class, args);
